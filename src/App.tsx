09e69187093f241aa76711b876b2eb3a18cf020a
Felipe <felipecsilva.dev@gmail.com>
2026-07-20 10:07:20 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
+import { useEffect } from 'react';
+
 import { InvestmentForm } from './components/InvestimentForm';
 import { InvestmentResult } from './components/InvestimentResult';
 
 import { useInvestmentCalculator } from './hooks/useInvestmentCalculator';
 
-
 export default function App() {
   const calculator = useInvestmentCalculator();
+
+  useEffect(() => {
+    document.title =
+      calculator.finalValue > 0
+        ? `Calculadora de Investimentos | 💰 ${calculator.finalValue.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}`
+        : 'Calculadora de Investimentos';
+  }, [calculator.finalValue]);
 
   return (
     <main>
       <InvestmentForm
         initialValue={calculator.initialValue}
-        monthlyContribution={
-          calculator.monthlyContribution
-        }
+        monthlyContribution={calculator.monthlyContribution}
         interestRate={calculator.interestRate}
         period={calculator.period}
         timeUnit={calculator.timeUnit}
         rateUnit={calculator.rateUnit}
-        onInitialValueChange={
-          calculator.setInitialValue
-        }
-        onMonthlyContributionChange={
-          calculator.setMonthlyContribution
-        }
-        onInterestRateChange={
-          calculator.setInterestRate
-        }
+        onInitialValueChange={calculator.setInitialValue}
+        onMonthlyContributionChange={calculator.setMonthlyContribution}
+        onInterestRateChange={calculator.setInterestRate}
         onPeriodChange={calculator.setPeriod}
         onTimeUnitChange={calculator.setTimeUnit}
         onRateUnitChange={calculator.setRateUnit}
